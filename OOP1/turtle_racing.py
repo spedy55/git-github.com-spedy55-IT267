b29201fad3689d435d3c437f1dce2fbcf02b9cd4
Thanawat Intrarajit <76065325+spedy55@users.noreply.github.com>
2022-06-20 13:25:50 +0700

    turtle

---
+++ OOP1/turtle_racing.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+from random import randint
+
+leo = Turtle('turtle')
+leo.color('blue')
+leo.penup()
+leo.goto(-160,100)
+leo.pendown()
+
+dona = Turtle('turtle')
+dona.color('purple')
+dona.penup()
+dona.goto(-160,70)
+dona.pendown()
+
+raph = Turtle('turtle')
+raph.color('red')
+raph.penup()
+raph.goto(-160,40)
+raph.pendown()
+
+mike = Turtle('turtle')
+mike.color('orange')
+mike.penup()
+mike.goto(-160,10)
+mike.pendown()
+
+for move in range(100):
+    leo.forward(randint(1,5))
+    dona.forward(randint(1,5))
+    raph.forward(randint(1,5))
+    mike.forward(randint(1,5))
